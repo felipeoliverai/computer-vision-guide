21b049fb3606bb7b1629790df7f59f2380b26b91
Felipe Oliveira <58439493+felipeoliverai@users.noreply.github.com>
2020-11-28 16:11:14 -0300

    Update argparse-02.py

--- frameworks/utils/argparse-02.py
+++ frameworks/utils/argparse-02.py
@@ -4,16 +4,12 @@ import argparse
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Testing")
-    parser.add_argument('--weights', type=str, default='model.h5',
-                        help='weights of trained neural network file')
-    parser.add_argument('--epochs', type=int, default=100,
-                        help='epochs for running training model')
-    parser.add_argument('--batch', type=int, default=16,
-                        help='batching size of samples for each epoch')
+    parser.add_argument('--weights', type=str, default='model.h5', help='weights of trained neural network file')
+    parser.add_argument('--epochs', type=int, default=100, help='epochs for running training model')
+    parser.add_argument('--batch', type=int, default=16,help='batching size of samples for each epoch')
 
     # store_true = boolean value
-    parser.add_argument('--pretrained', action='store_true',
-                        help='Setup way to train pre-trained architecture')
+    parser.add_argument('--pretrained', action='store_true', help='Setup way to train pre-trained architecture')
     args = parser.parse_args()
 
     # visualize results
