d3ad52f0ced130b11e5af368d90e168a79dc4055
Felipe Oliveira <58439493+felipeoliverai@users.noreply.github.com>
2020-11-28 13:04:27 -0300

    Add files via upload

---
+++ frameworks/utils/argparse-01.py
@@ -0,0 +1,26 @@
+import argparse
+
+print(argparse.__version__)
+
+
+parser = argparse.ArgumentParser(description="Math equation")
+
+
+# add arguments with method add_argument()
+parser.add_argument('-v1', '--value1', type=int,
+                    help="The first value of equation")
+parser.add_argument('-v2', '--value2', type=int,
+                    help="Second value of equation")
+
+
+# parsing arguments
+args = parser.parse_args()
+
+
+def equation(value1, value2):
+    calc = value1 * value2
+    return calc
+
+
+if __name__ == "__main__":
+    print('The result is: {}'.format(equation(args.value1, args.value2)))
